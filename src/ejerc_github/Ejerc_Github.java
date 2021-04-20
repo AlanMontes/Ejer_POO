e96bf3a53c4435e743c24992b5a0b07f17e6a3e2
hp <hp@LAPTOP-SK7I2D30>
2021-04-19 23:54:05 -0600

    Programa que muestra nombre y sus iniciales

--- src/ejerc_github/Ejerc_Github.java
+++ src/ejerc_github/Ejerc_Github.java
@@ -4,25 +4,25 @@ public class Ejerc_Github{
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);  
         int Niniciales = 1;
-        String iniciales = "";
+        String iniciales = " ";
         
         System.out.println("Bienvenido, por favor introduzca su nombre completo, separado por espacios '1 entre cada nombre o apellido': ");
         System.err.println("EJEMPLO: Alan Jesus Montes Silva");
         String nombre = sc.nextLine();
         System.out.println("Su nombre es: "+nombre);                    
-          //Lo comentado es una variante del programa que muestra el numero de la inicial y esta misma
-          //System.out.println("");
-          /*  if(nombre.charAt(0) != ' '){
+           
+            System.out.println("");
+            if(nombre.charAt(0) != ' '){
             System.out.println("Su inicial "+ Niniciales +" es : "+nombre.charAt(0));
-          } */        
+          }         
        for(int i = 0; i < nombre.length(); i++){         
            if(nombre.charAt(i) == ' '){
-               //System.out.println("Su inicial "+ Niniciales +" es : "+nombre.charAt(i+1)); 
+               System.out.println("Su inicial "+ Niniciales +" es : "+nombre.charAt(i+1)); 
                iniciales = iniciales+" "+nombre.charAt(i+1);
                Niniciales++;
            }
        } 
-        //System.out.println("");
+        System.out.println("");
         if(nombre.charAt(0) != ' '){
             String ini = nombre.charAt(0)+iniciales;
             System.out.println("INICIALES: "+ini.toUpperCase());}
